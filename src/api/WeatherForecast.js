8a679a127029fe4fdfcaf149a0d77ac7389406de
taurus-git <t.1113@ya.ru>
2021-06-02 16:47:28 +0300

    Deleted unused variable

--- src/api/WeatherForecast.js
+++ src/api/WeatherForecast.js
@@ -3,10 +3,9 @@ import axios from "axios";
 let WeatherForecast = async (city) => {
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-    let weatherData = await axios.get(
+    return await axios.get(
         `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=16&APPID=${API_KEY}`
     )
-    return weatherData;
 }
 
 export default WeatherForecast;

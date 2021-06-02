ff581532122352395acfa5dcc81fc478062720cc
taurus-git <t.1113@ya.ru>
2021-06-02 16:46:34 +0300

    Changed api url.

--- src/api/WeatherForecast.js
+++ src/api/WeatherForecast.js
@@ -4,7 +4,7 @@ let WeatherForecast = async (city) => {
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
     let weatherData = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`
+        `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=16&APPID=${API_KEY}`
     )
     return weatherData;
 }
